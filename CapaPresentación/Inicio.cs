f6e448e93153966497cc6eaad0e52df2f57df4e9
gablanco2023 <e_gablanco@latam.stefanini.com>
2023-05-26 15:16:07 -0500

    Agregar archivos de proyecto.

---
+++ CapaPresentación/Inicio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentación
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void iconMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconMenuItem7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CapaPresentación/Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentación
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Inicio form = new Inicio();
+
+            form.Show();
+            this.Hide();
+
+            form.FormClosing += frm_clossing;
+
+        }
+
+        private void frm_clossing(object sender, FormClosingEventArgs e)
+        {
+            txtDocument.Text = "";
+            txtClave.Text = "";
+
+            this.Show();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
